23b999e7c921a877b8fddde9deb6fe16d4b6c000
Icarus02423 <156101940+Icarus02423@users.noreply.github.com>
2024-09-09 20:20:15 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);;
         FizzBuzz.main(new String[0]);
-
+        int w = 99;
+        println(w)
     }
 }
